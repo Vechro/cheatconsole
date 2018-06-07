277f6ba7f186548c9f2e6887837ef9a0baa16927
Vechro <stenmr@gmail.com>
2018-06-07 11:59:05 +0300

    More functions
    Waypoint is now a function and also works properly, more functions.

--- cheatconsole/cheatconsole.lua
+++ cheatconsole/cheatconsole.lua
@@ -1,10 +1,3 @@
-local function ShowNotification(text) -- display a notification above the radar
-    SetNotificationTextEntry("STRING")
-    AddTextComponentString(text)
-    DrawNotification(false, false)
-    print(text) -- also print in the console for recordkeeping
-end
-
 Citizen.CreateThread(function()
 
     local function KeyboardInput(textentry, exampletext, maxstringlength)
@@ -32,6 +25,45 @@ Citizen.CreateThread(function()
             blockInput = false
             return nil
         end
+    end
+
+    local function ShowNotification(text) -- display a notification above the radar
+        SetNotificationTextEntry("STRING")
+        AddTextComponentString(text)
+        DrawNotification(false, false)
+        print(text) -- also print in the console for recordkeeping
+    end
+
+    local function teleportOnGround(ped, x, y) -- teleport to waypoint
+        Citizen.CreateThread(function()
+            local zTestHeight = 0.0
+            local height = 990.0
+            while true do
+            Citizen.Wait(5)
+
+                if IsPedInAnyVehicle(ped, false) and (GetPedInVehicleSeat(GetVehiclePedIsIn(ped, false), -1) == ped) then
+                    entity = GetVehiclePedIsIn(ped, false)
+                else
+                    entity = ped
+                end
+
+                while not RequestCollisionAtCoord(x + 0.0, y + 0.0, height) do 
+                    Citizen.Wait(5)
+                end
+
+                if zTestHeight == 0.0 then
+                    height = height - 30.0
+                    while not RequestCollisionAtCoord(x + 0.0, y + 0.0, height) do 
+                        Citizen.Wait(5)
+                    end
+                    result, zTestHeight = GetGroundZFor_3dCoord(x + 0.0, y + 0.0, height + 0.0, Citizen.ReturnResultAnyway())
+                else
+                    SetEntityCoords(entity, x + 0.0, y + 0.0, zTestHeight)
+                    ShowNotification(("Teleported to X: %.4f; Y: %.4f; Z: %.4f"):format(x, y, zTestHeight))
+                    return
+                end
+            end
+        end)
     end
     
     local function UpgradeVehicle(vehicle)
@@ -64,65 +96,90 @@ Citizen.CreateThread(function()
                     table.insert(splitTable, word)
                 end
 
+                local first, second, third = table.unpack(splitTable)
+
                 local playerPed = PlayerPedId()
 
-                if tonumber(splitTable[1]) and tonumber(splitTable[2]) and tonumber(splitTable[3]) then
+                if tonumber(first) and tonumber(second) and tonumber(third) then
                     if IsPedInAnyVehicle(playerPed, false) and (GetPedInVehicleSeat(GetVehiclePedIsIn(playerPed, false), -1) == playerPed) then
                         entity = GetVehiclePedIsIn(playerPed, false)
                     else
                         entity = playerPed
                     end
-                    SetEntityCoords(entity, tonumber(splitTable[1]) + 0.0, tonumber(splitTable[2]) + 0.0, tonumber(splitTable[3]) + 0.0, true, false, false, true)
-                    ShowNotification("Teleported to X: " .. splitTable[1] .. " Y: " .. splitTable[2] .. " Z: " .. splitTable[3])
+                    SetEntityCoords(entity, tonumber(first) + 0.0, tonumber(second) + 0.0, tonumber(third) + 0.0, true, false, false, true)
+                    ShowNotification("Teleported to X: " .. first .. "; Y: " .. second .. "; Z: " .. third)
 
-                elseif splitTable[1] == "upgrade" then
+                elseif first == "upgrade" then
                     if IsPedInAnyVehicle(playerPed, false) then
                         local model = GetVehiclePedIsIn(playerPed, false)
                         UpgradeVehicle(model)
                         ShowNotification(GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(model))) .. " upgraded")
+                        ShowNotification(GetDisplayNameFromVehicleModel(GetEntityModel(model)) .. " upgraded")
                     else
                         ShowNotification("You're not in a vehicle")
                     end
 
-                elseif splitTable[1] == "suicide" then
+                elseif first == "suicide" then
                     SetEntityHealth(playerPed, 0)
 
-                elseif splitTable[1] == "max" and splitTable[2] == "stats" then
+                elseif first == "max" and second == "stats" then
                     for index, stat in ipairs(statTable) do
                         StatSetInt(GetHashKey(stat), 100, true)
                     end
                     ShowNotification("All stats maxed out")
 
-                elseif splitTable[1] == "check" and splitTable[2] == "stats" then
+                elseif first == "check" and second == "stats" then
                     for index, stat in ipairs(statTable) do
                         local _, statValue = StatGetInt(GetHashKey(stat), -1, -1)
                         print(stat .. ": " .. statValue)
                     end
                 
-                -- elseif splitTable[1] == "fix" or splitTable[2] == "repair" then
+                elseif first == "fix" or first == "repair" then
+                    if IsPedInAnyVehicle(playerPed, false) then
+                        local vehicle = GetVehiclePedIsIn(playerPed, false)
+                        SetVehicleFixed(vehicle)
+                        SetVehicleDirtLevel(vehicle, 0.0)
+                        ShowNotification(GetLabelText(GetDisplayNameFromVehicleModel(GetEntityModel(vehicle))) .. " cleaned and repaired")
+                    else
+                        ShowNotification("You're not in a vehicle")
+                    end
 
+                elseif first == "remove" then
+                    if second and IsWeaponValid(GetHashKey(second)) then
+                        RemoveWeaponFromPed(playerPed, GetHashKey(second))
+                        ShowNotification(second .. " removed")
+                    elseif second and IsWeaponValid(GetHashKey("weapon_" .. second)) then
+                        RemoveWeaponFromPed(playerPed, GetHashKey("weapon_" .. second))
+                        ShowNotification("weapon_" .. second .. " removed")
+                    elseif not second then
+                        local _, currentWeapon = GetCurrentPedWeapon(playerPed)
+                        RemoveWeaponFromPed(playerPed, currentWeapon, true)
+                        ShowNotification("Current weapon removed")
+                    else
+                        ShowNotification("Unknown weapon")
+                    end
 
-                elseif splitTable[1] == "remove" and splitTable[2] == "all" and splitTable[3] == "weapons" then
+                elseif first == "remove" and second == "all" and third == "weapons" then
                     RemoveAllPedWeapons(playerPed, true)
                     ShowNotification("All weapons removed")
 
-                elseif splitTable[1] == "waypoint" then -- 60% of the time, it works every time
+                elseif first == "waypoint" then
                     local WaypointHandle = GetFirstBlipInfoId(8)
                     if DoesBlipExist(WaypointHandle) then
                         wpcoord = Citizen.InvokeNative(0xFA7C7F0AADF25D09, WaypointHandle, Citizen.ResultAsVector())
-                        waypoint = true
+                        teleportOnGround(playerPed, wpcoord.x, wpcoord.y)
                     else
                         ShowNotification("No waypoint marked")
                     end
 
-                elseif splitTable[1] == "get" and splitTable[2] == "coords" then
+                elseif first == "get" and (second == "coord" or second == "coords") then
                     local coord = GetEntityCoords(playerPed)
                     ShowNotification(("You're at X: %.4f; Y: %.4f; Z: %.4f"):format(coord.x, coord.y, coord.z))
 
                 else
-                    if IsModelValid(GetHashKey(splitTable[1])) then
-                        local model = GetHashKey(splitTable[1])
-                        local x, y, z = table.unpack(GetEntityCoords(playerPed, true)) -- to be replaced with just accessing a table
+                    if IsModelValid(GetHashKey(first)) then
+                        local model = GetHashKey(first)
+                        local coord = GetEntityCoords(playerPed, true)
                         RequestModel(model)
                         while not HasModelLoaded(model) do
                             Wait(0)
@@ -132,83 +189,46 @@ Citizen.CreateThread(function()
                             SetEntityAsMissionEntity(vehicle, true, true)
                             DeleteEntity(vehicle)
                         end
-                        local vehicle = CreateVehicle(model, x, y, z, GetEntityHeading(playerPed), true, true)
+                        local vehicle = CreateVehicle(model, coord.x, coord.y, coord.z, GetEntityHeading(playerPed), true, true)
                         SetPedIntoVehicle(playerPed, vehicle, -1)
                         UpgradeVehicle(vehicle)
                         SetEntityAsNoLongerNeeded(vehicle)
                         ShowNotification(GetLabelText(GetDisplayNameFromVehicleModel(model)) .. " spawned")
 
-                    elseif splitTable[1] and (IsWeaponValid(GetHashKey(splitTable[1])) or IsWeaponValid(GetHashKey("weapon_" .. splitTable[1])) or IsWeaponValid(GetHashKey("gadget_" .. splitTable[1]))) then
-                        -- if splitTable[1] == "parachute" or splitTable[1] == "nightvision" or splitTable[1] == "gadget_parachute" or splitTable[1] == "gadget_nightvision" then
-                        if gadgets[splitTable[1]] then
-                            if IsWeaponValid(GetHashKey(splitTable[1])) then
-                                GiveWeaponToPed(playerPed, splitTable[1], 1, false, false)
-                                ShowNotification(splitTable[1] .. " given")
+                    elseif first and (IsWeaponValid(GetHashKey(first)) or IsWeaponValid(GetHashKey("weapon_" .. first)) or IsWeaponValid(GetHashKey("gadget_" .. first))) then
+
+                        if gadgets[first] then
+                            if IsWeaponValid(GetHashKey(first)) then
+                                GiveWeaponToPed(playerPed, first, 1, false, false)
+                                ShowNotification(first .. " given")
                             else
-                                GiveWeaponToPed(playerPed, "gadget_" .. splitTable[1], 1, false, false)
-                                ShowNotification("gadget_" .. splitTable[1] .. " given")
+                                GiveWeaponToPed(playerPed, "gadget_" .. first, 1, false, false)
+                                ShowNotification("gadget_" .. first .. " given")
                             end
+
                         else
-                            if IsWeaponValid(GetHashKey(splitTable[1])) then
-                                GiveWeaponToPed(playerPed, splitTable[1], tonumber(splitTable[2]) or 9999, false, false)
-                                if splitTable[2] and tonumber(splitTable[2]) < 0 then
-                                    ShowNotification(splitTable[1] .. " given with infinite ammo")
+                            if IsWeaponValid(GetHashKey(first)) then
+                                GiveWeaponToPed(playerPed, first, tonumber(second) or 9999, false, false)
+                                if second and tonumber(second) < 0 then
+                                    ShowNotification(first .. " given with infinite ammo")
                                 else
-                                    ShowNotification((tonumber(splitTable[2]) and "" .. splitTable[1] .. " given with " .. splitTable[2] .. " ammo") or splitTable[1] .. " given")
+                                    ShowNotification((tonumber(second) and "" .. first .. " given with " .. second .. " ammo") or first .. " given")
                                 end
                             else
-                                GiveWeaponToPed(playerPed, "weapon_" .. splitTable[1], tonumber(splitTable[2]) or 9999, false, false)
-                                if splitTable[2] and tonumber(splitTable[2]) < 0 then
-                                    ShowNotification("weapon_" .. splitTable[1] .. " given with infinite ammo")
+                                GiveWeaponToPed(playerPed, "weapon_" .. first, tonumber(second) or 9999, false, false)
+                                if second and tonumber(second) < 0 then
+                                    ShowNotification("weapon_" .. first .. " given with infinite ammo")
                                 else
-                                    ShowNotification((tonumber(splitTable[2]) and "weapon_" .. splitTable[1] .. " given with " .. splitTable[2] .. " ammo") or "weapon_" .. splitTable[1] .. " given")
+                                    ShowNotification((tonumber(second) and "weapon_" .. first .. " given with " .. second .. " ammo") or "weapon_" .. first .. " given")
                                 end
                             end
                         end
 
                     else
-                        if not splitTable[1] then
-                        else
-                            ShowNotification("Failed to interpret command")
-                        end
+                        ShowNotification("Failed to interpret command")
                     end
                 end
                 splitTable = {}
-            end
-        end
-    end
-end)
-
-Citizen.CreateThread(function() -- teleport to waypoint
-    local zTestHeight = 0.0
-    local height = 1000.0
-    local playerPed = PlayerPedId()
-	while true do
-	Citizen.Wait(5)
-        if waypoint then
-            if IsPedInAnyVehicle(playerPed, false) and (GetPedInVehicleSeat(GetVehiclePedIsIn(playerPed, false), -1) == playerPed) then
-                entity = GetVehiclePedIsIn(playerPed, false)
-            else
-                entity = playerPed
-            end
-
-            while not RequestCollisionAtCoord(wpcoord.x + 0.0, wpcoord.y + 0.0, height) do 
-                Citizen.Wait(5)
-            end
-            print("stage 1")
-            if zTestHeight == 0.0 then
-                height = height - 30.0
-                while not RequestCollisionAtCoord(wpcoord.x + 0.0, wpcoord.y + 0.0, height) do 
-                    Citizen.Wait(5)
-                end
-                result, zTestHeight = GetGroundZFor_3dCoord(wpcoord.x + 0.0, wpcoord.y + 0.0, height + 0.0, Citizen.ReturnResultAnyway())
-                print("stage 2")
-            else
-                SetEntityCoords(entity, wpcoord.x + 0.0, wpcoord.y + 0.0, zTestHeight)
-                waypoint = false
-                ShowNotification(("Teleported to X: %.4f; Y: %.4f; Z: %.4f"):format(wpcoord.x, wpcoord.y, zTestHeight))
-                zTestHeight = 0.0
-                height = 1000.0
             end
         end
     end
